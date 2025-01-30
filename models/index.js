7d98ba1eb43b938964fec84619e97bce03d49581
DonHansCL <hansenglishteacher@gmail.com>
2025-01-30 17:18:32 -0300

    Cambios en server.js conexion a render

--- models/index.js
+++ models/index.js
@@ -23,8 +23,18 @@ if (env === 'production') {
     },
     logging: false
   });
+} else if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+      ...config,
+      logging: false
+    }
+  );
 }
 
 fs
