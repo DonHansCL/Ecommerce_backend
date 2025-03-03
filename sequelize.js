0cccf294494fdefd9aea5faaf2a4215958ccfc36
DonHansCL <hansenglishteacher@gmail.com>
2025-03-03 15:11:26 -0300

    sequalize.js actualizado

--- sequelize.js
+++ sequelize.js
@@ -1,8 +1,23 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`, {
-  dialect: 'postgres',
-});
+let sequelize;
+
+// Usar DATABASE_URL si está disponible, de lo contrario usar los parámetros individuales
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else {
+  sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`, {
+    dialect: 'postgres',
+  });
+}
 
 module.exports = sequelize;
